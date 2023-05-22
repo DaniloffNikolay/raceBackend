7841dbf15819913482bbe30d43c65e9c4193736e
Nikolai <89162450327@ya.com>
2023-05-22 10:28:58 +0300

    first commit

---
+++ src/main/java/ru/danilov/racebackend/RaceBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.danilov.racebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/danilov/racebackend/RaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.danilov.racebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
